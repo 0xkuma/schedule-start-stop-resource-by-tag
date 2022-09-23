af69a81a21ce6540fe49802b8fce7c1b49849c2b
Eddie Cheung <eddie.cheung@ecloudvalley.com>
2022-09-23 22:53:10 +0800

    Add permission for event bridge trigger

--- lambda.tf
+++ lambda.tf
@@ -3,7 +3,7 @@ resource "null_resource" "zipLambda" {
     timestamp = timestamp()
   }
   provisioner "local-exec" {
-    command = "cd app && zip -r lambda.zip *.py "
+    command = "cd app/dist && rm -rf * && cp ../*.py . && pip3 install -r ../requirements.txt -t . && zip -r lambda.zip *"
   }
 }
 
@@ -13,9 +13,17 @@ resource "aws_lambda_function" "schedule_start_stop_resource" {
   role          = "arn:aws:iam::066529494542:role/POC-LambdaRole"
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.8"
-  filename      = "./app/lambda.zip"
+  filename      = "./app/dist/lambda.zip"
   source_code_hash = filebase64sha256("./app/lambda_function.py")
   tags = {
     "Name" = "schedule-start-stop-resource"
   }
+}
+
+resource "aws_lambda_permission" "allow_event_bridge" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.schedule_start_stop_resource.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.schedule_start_resource_rule.arn
 }
