a2fd0d1b613f96c570127d0ba030e07c726623c9
Eddie Cheung <eddie.cheung@ecloudvalley.com>
2022-09-23 22:52:48 +0800

    Add stop resource event bridge

--- eventBridge.tf
+++ eventBridge.tf
@@ -1,14 +1,29 @@
-resource "aws_cloudwatch_event_rule" "schedule_start_resource" {
+resource "aws_cloudwatch_event_rule" "schedule_start_resource_rule" {
   name                = "schedule-start-resource"
   description         = "EventBridge Rule"
   schedule_expression = "rate(5 minutes)"
 }
 
-resource "aws_cloudwatch_event_target" "eventBridgeTarget" {
-  rule      = aws_cloudwatch_event_rule.schedule_start_resource.name
+resource "aws_cloudwatch_event_rule" "schedule_stop_resource_rule" {
+  name                = "schedule-stop-resource"
+  description         = "EventBridge Rule"
+  schedule_expression = "rate(5 minutes)"
+}
+
+resource "aws_cloudwatch_event_target" "schedule_start_resource_target" {
+  rule      = aws_cloudwatch_event_rule.schedule_start_resource_rule.name
   target_id = "schedule-start-resource"
   arn       = aws_lambda_function.schedule_start_stop_resource.arn
   input = jsonencode({
     "job" = "start"
+  })
+}
+
+resource "aws_cloudwatch_event_target" "schedule_stop_resource_target" {
+  rule      = aws_cloudwatch_event_rule.schedule_stop_resource_rule.name
+  target_id = "schedule_stop-resource"
+  arn       = aws_lambda_function.schedule_start_stop_resource.arn
+  input = jsonencode({
+    "job" = "stot"
   })
 }
