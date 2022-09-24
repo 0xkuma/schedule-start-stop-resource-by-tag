7ba8ff1aff86d7cfe355e0d3d420676073c5d56c
Eddie Cheung <eddie.cheung@ecloudvalley.com>
2022-09-24 19:28:46 +0800

    Update lambda timeout & memory size

--- lambda.tf
+++ lambda.tf
@@ -13,6 +13,8 @@ resource "aws_lambda_function" "schedule_start_stop_resource" {
   role          = aws_iam_role.labmda_role.arn
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.8"
+  timeout       = 300
+  memory_size   = 2048
   filename      = "./app/dist/lambda.zip"
   source_code_hash = filebase64sha256("./app/lambda_function.py")
   tags = {
