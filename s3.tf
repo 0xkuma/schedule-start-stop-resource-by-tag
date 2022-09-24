4c614ca0bebc13397566b14e4bb6bc61cc8bffb8
Eddie Cheung <eddie.cheung@ecloudvalley.com>
2022-09-25 01:03:32 +0800

    Add s3 resource for lambda source code

---
+++ s3.tf
@@ -0,0 +1,18 @@
+resource "aws_s3_bucket" "artifact" {
+  bucket = "schedule-start-stop-resource-by-tag"
+  tags = {
+    Name = "schedule-start-stop-resource-by-tag"
+  }
+}
+
+resource "aws_s3_bucket_acl" "acl" {
+  bucket = aws_s3_bucket.artifact.id
+  acl    = "private"
+}
+
+resource "aws_s3_object" "lambda_zip" {
+  bucket = aws_s3_bucket.artifact.id
+  key    = "lambda.zip"
+  source = data.archive_file.zip.output_path
+  etag = filemd5(data.archive_file.zip.output_path)
+}
