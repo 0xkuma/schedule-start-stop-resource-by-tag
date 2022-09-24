4dc26edb7542f185fb3256e56607f9420bcbc87c
Eddie Cheung <eddie.cheung@ecloudvalley.com>
2022-09-24 19:27:17 +0800

    Add rds start/stop function

--- app/resourceController.py
+++ app/resourceController.py
@@ -1,4 +1,3 @@
-# export startEc2Instace function
 def startEc2Instace(ec2, instanceIdList):
   response = ec2.start_instances(
     InstanceIds=instanceIdList,
@@ -12,3 +11,15 @@ def stopEc2Instace(ec2, instanceIdList):
     DryRun=False
   )
   return response
+
+def startRdsInstace(rds, instanceIdList):
+  for instanceId in instanceIdList:
+    rds.start_db_instance(
+      DBInstanceIdentifier=instanceId,
+    )
+
+def stopRdsInstace(rds, instanceIdList):
+  for instanceId in instanceIdList:
+    rds.stop_db_instance(
+      DBInstanceIdentifier=instanceId,
+    )
