6858911abf0a0bfd04038ba8bcbb41be7cd53631
Eddie Cheung <eddie.cheung@ecloudvalley.com>
2022-09-25 01:09:06 +0800

    Update lambda source code & clear env

--- lambda.tf
+++ lambda.tf
@@ -1,22 +1,23 @@
-resource "null_resource" "zipLambda" {
+resource "null_resource" "pre_build" {
   triggers = {
     timestamp = timestamp()
   }
   provisioner "local-exec" {
-    command = "cd app/dist && rm -rf * && cp ../*.py . && pip3 install -r ../requirements.txt -t . && zip -r lambda.zip *"
+    command = "cd app/dist && rm -rf * && cp ../*.py . && pip3 install -r ../requirements.txt -t ."
   }
 }
 
 resource "aws_lambda_function" "schedule_start_stop_resource" {
-  depends_on    = [null_resource.zipLambda]
+  depends_on    = [null_resource.pre_build, data.archive_file.zip]
   function_name = "schedule-start-stop-resource"
   role          = aws_iam_role.labmda_role.arn
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.8"
   timeout       = 300
   memory_size   = 2048
-  filename      = "./app/dist/lambda.zip"
-  source_code_hash = filebase64sha256("./app/lambda_function.py")
+  s3_bucket     = aws_s3_bucket.artifact.id
+  s3_key        = aws_s3_object.lambda_zip.key
+  source_code_hash = data.archive_file.zip.output_base64sha256
   tags = {
     "Name" = "schedule-start-stop-resource"
   }
@@ -36,4 +37,21 @@ resource "aws_lambda_permission" "allow_stop_event_bridge" {
   function_name = aws_lambda_function.schedule_start_stop_resource.function_name
   principal     = "events.amazonaws.com"
   source_arn    = aws_cloudwatch_event_rule.schedule_stop_resource_rule.arn
+}
+
+data "archive_file" "zip" {
+  depends_on  = [null_resource.pre_build]
+  type        = "zip"
+  source_dir  = "${path.module}/app/dist"
+  output_path = "${path.module}/app/dist/lambda.zip"
+}
+
+resource "null_resource" "clear_env" {
+  depends_on = [aws_s3_object.lambda_zip]
+  triggers = {
+    timestamp = timestamp()
+  }
+  provisioner "local-exec" {
+    command = "rm -rf ${path.module}/app/dist/*"
+  }
 }
