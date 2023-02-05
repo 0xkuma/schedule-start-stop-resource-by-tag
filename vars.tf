61649c26faad01b5904d409b655e077f0ea6ceb8
Eddie Cheung <eddie.cheung@ecloudvalley.com>
2023-02-05 13:22:55 +0800

    remove var

--- vars.tf
+++ vars.tf
@@ -1,11 +1,11 @@
-variable "AWS_ACCESS_KEY" {
-  description = "The AWS Access Key for the account"
-}
+# variable "AWS_ACCESS_KEY" {
+#   description = "The AWS Access Key for the account"
+# }
 
-variable "AWS_SECRET_KEY" {
-  description = "The AWS Secret Key for the account"
-}
+# variable "AWS_SECRET_KEY" {
+#   description = "The AWS Secret Key for the account"
+# }
 
-variable "AWS_REGION" {
-  description = "The AWS Region for the deployment"
-}
+# variable "AWS_REGION" {
+#   description = "The AWS Region for the deployment"
+# }
