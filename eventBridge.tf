115066d599244e59c4eb9e05d3808205523f7b0a
Eddie Cheung <eddie.cheung@ecloudvalley.com>
2023-02-06 16:57:09 +0800

    Update schedule expression

--- eventBridge.tf
+++ eventBridge.tf
@@ -1,13 +1,13 @@
 resource "aws_cloudwatch_event_rule" "schedule_start_resource_rule" {
   name                = "schedule-start-resource"
   description         = "EventBridge Rule"
-  schedule_expression = "0 * * * *"
+  schedule_expression = "cron(0 * * * ? *)"
 }
 
 resource "aws_cloudwatch_event_rule" "schedule_stop_resource_rule" {
   name                = "schedule-stop-resource"
   description         = "EventBridge Rule"
-  schedule_expression = "0 * * * *"
+  schedule_expression = "cron(0 * * * ? *)"
 }
 
 resource "aws_cloudwatch_event_target" "schedule_start_resource_target" {
